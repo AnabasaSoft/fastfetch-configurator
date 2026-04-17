27d53d7620598dbb4065204a1174798249a491e0
AnabasaSoft <anabasasoft@gmail.com>
2026-04-17 05:52:33 +0200

    Update to v1.2: Refactorizado a paquete nativo Python según normas de AUR

--- main.py
+++ main.py
@@ -252,7 +252,7 @@ def resource_path(relative_path):
         # PyInstaller crea una carpeta temporal y guarda la ruta en _MEIPASS
         base_path = sys._MEIPASS
     except Exception:
-        base_path = os.path.abspath(".")
+        base_path = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(base_path, relative_path)
 
 class DialogoEditarModulo(QDialog):
